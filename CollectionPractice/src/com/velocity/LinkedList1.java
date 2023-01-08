2d5dfa57368dfb12ba42531d14f0038c0df9d5f4
priyankachachar <pchikane47@gmail.com>
2023-01-08 17:28:01 +0530

    Linkedlist1added

---
+++ CollectionPractice/src/com/velocity/LinkedList1.java
@@ -0,0 +1,18 @@
+package com.velocity;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedList1 {
+public static void main(String[] args) {
+	List<Integer> list=new LinkedList<Integer>();
+	list.add(10);
+	list.add(20);
+	list.add(30);
+	for(Integer i:list)
+	{
+		System.out.println(i);
+		
+	}
+}
+}
